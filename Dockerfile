eba6bd3d233a887152c9c72ce65e175eb3585419
Yukming <moondrashiwa@gmail.com>
2023-12-06 21:42:04 +0100

    Update Dockerfile and gunicorn_config.py

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY ./requirements.txt /app
 
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
-EXPOSE 8080
+EXPOSE 8000
 
 CMD ["gunicorn","--config", "gunicorn_config.py", "app:app"]
--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -6,7 +6,7 @@ threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8000')
 
 forwarded_allow_ips = '*'
 
