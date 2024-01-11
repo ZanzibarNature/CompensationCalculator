d72d65903526217d30118ca229c1f700ecbf3b39
Yukming <moondrashiwa@gmail.com>
2024-01-11 16:21:20 +0100

    Add Swagger documentation for GetCompensationAmount API

--- app/app.py
+++ app/app.py
@@ -4,12 +4,56 @@ from flask import request
 from dotenv import load_dotenv
 from service.DistanceService import DistanceService
 from service.CompensationService import CompensationService
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 load_dotenv()
 
 @app.route("/getCompensationAmount", methods=["GET"])
 def GetCompensationAmount():
+    """Get compensation amount
+    ---
+    parameters:
+      - name: lonFrom
+        in: query
+        type: string
+        required: true
+      - name: latFrom
+        in: query
+        type: string
+        required: true
+      - name: lonTo
+        in: query
+        type: string
+        required: true
+      - name: latTo
+        in: query
+        type: string
+        required: true
+      - name: toCurrency
+        in: query
+        type: string
+        required: true
+    responses:
+        200:
+            description: Compensation amount
+            schema:
+            id: compensation
+            properties:
+                co2FootprintInKG:
+                type: float
+                description: The co2 footprint in kg
+                default: 0
+                totalCost:
+                type: float
+                description: The total cost
+                default: 0
+                currency:
+                type: string
+                description: The currency
+                default: EUR
+    """
     distanceService = DistanceService()
     compensationService = CompensationService()
 
